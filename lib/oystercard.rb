55890bbe0a9e3b1311058f9880ba6d89addc841b
Matt Burstein <j.matthew.burstein@gmail.com>
2018-01-29 16:52:42 +0000

    Add error handling for Oystercard#top_up when maximum balance exceeded

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,22 @@
 class Oystercard
 
   DEFAULT_BALANCE = 5
-  attr_reader :balance 
+  DEFAULT_LIMIT = 90
+  attr_reader :balance
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+  end
+
+  def top_up(amount)
+    raise "Maximum balance of #{DEFAULT_LIMIT} exceeded" if limit_reached?(amount)
+    @balance += amount
+  end
+
+  private
+
+  def limit_reached?(amount)
+    (@balance + amount) > DEFAULT_LIMIT
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,4 +14,19 @@ describe Oystercard do
       expect(oystercard.balance).to eq Oystercard::DEFAULT_BALANCE
     end
   end
+
+  describe "#top_up" do
+    it "tops up the oyster card" do
+      top_up_amount = 10
+      expect{ subject.top_up(top_up_amount)}.to change{subject.balance}.by(top_up_amount)
+    end
+
+    context "when balance exceeds maximum limit" do
+      it "raises 'Maximum balance exceeded' error" do
+        error_message = "Maximum balance of #{Oystercard::DEFAULT_LIMIT} exceeded"
+        top_up_amount = Oystercard::DEFAULT_LIMIT - subject.balance + 1
+        expect{ subject.top_up(top_up_amount) }.to raise_error error_message
+      end
+    end
+  end
 end
