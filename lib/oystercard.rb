34afe12a65d94043de46e0feb391616d96bbb438
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-01-29 16:23:43 +0000

    Created tests and methods for initialize and balance. Pass

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,10 @@
 class Oystercard
 
+  DEFAULT_BALANCE = 5
+  attr_reader :balance 
+
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,17 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard) {described_class.new}
 
+  context "when initialize is passed argument" do
+    subject(:oystercard) { described_class.new(20) }
+    it "balance equals argument" do
+      expect(oystercard.balance).to eq 20
+    end
+  end
+  context "when initialize not passed argument" do
+    it "balance equals default balance" do
+      expect(oystercard.balance).to eq Oystercard::DEFAULT_BALANCE
+    end
+  end
 end
